1b59dce54eac001024db34feb46da316d74e17fb
AKSHAT SHAH <ug201210032@iitj.ac.in>
2016-06-16 12:45:34 +0530

    third failure

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,5 +2,13 @@ require_relative '../fizzbuzz.rb'
 require 'spec_helper.rb'
 
 describe Fizzbuzz do
+  before :each do
+    @fizzbuzz = Fizzbuzz.new "up_bound"
+  end
 
+  describe "#new" do
+    it "takes upperbound and returns it" do
+      @fizzbuzz.shoule be_an_instance_of Fizzbuzz
+    end
+  end
 end
