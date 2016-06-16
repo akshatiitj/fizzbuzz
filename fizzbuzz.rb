caf4d7aedffde22d3202c000884fec81b9e1bd0c
akshatiitj <ug201210032@iitj.ac.in>
2016-06-16 15:46:12 +0530

    classify tested and passed

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,32 @@
 class Fizzbuzz
-  attr_accessor :up_bound
+  attr_accessor :up_bound, :low_bound
   def initialize(low_bound = 1, up_bound = 100)
     @up_bound = up_bound
     @low_bound = low_bound
   end
   
-  def iterate_print
+  def iterate
+    (@low_bound..@up_bound).step(1).each do |element|
+      print_to_screen(element)
+    end
+  end
+
+  def print_to_screen(element)
+    p self.classify(element)
+  end  
+
+  def classify(number)
+    if number % 3 == 0 and number % 5 == 0
+      return "FizzBuzz"
+    elsif number % 3 == 0
+      return "Fizz"
+    elsif number % 5 == 0
+      return "Buzz"
+    else
+      return number
+    end
   end
 end
 
-#new_fizz = Fizzbuzz.new(12, 50)
+#new_fizz = Fizzbuzz.new(1, 100)
+#new_fizz.iterate
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,19 +2,40 @@ require_relative '../fizzbuzz.rb'
 require 'spec_helper.rb'
 
 describe Fizzbuzz do
-  before :each do
-    @fizzbuzz = Fizzbuzz.new "low_bound", "up_bound"
-  end
+  let(:fizzbuzz) { Fizzbuzz.new "low_bound", "up_bound" }
 
   describe "#new" do
     it "takes upperbound and returns it" do
-      @fizzbuzz.should be_an_instance_of Fizzbuzz
+      fizzbuzz.should be_an_instance_of Fizzbuzz
     end
   end
 
-#  describe "#list_return" do
- #   it "returns a list from 1 to 100" do
-  #    @fizzbuzz.output.should eql (1..100).step(1).to_a
-   # end
-  #end
+  describe "#classify" do
+    context "FizzBuzz"
+    it "checks for multiples of 15" do
+      fizzbuzz.classify(15).should eq_to "FizzBuzz"
+    end
+  end
+
+  describe "#classify" do
+    context "Fizz"
+    it "checks for multiples of 3" do
+      fizzbuzz.classify(12).should eq_to "Fizz"
+    end
+  end
+
+  describe "#classify" do
+    context "Buzz"
+    it "checks for multiples of 5" do
+      fizzbuzz.classify(20).should eq_to "Buzz"
+    end
+  end
+
+  describe "#classify" do
+    context "Other numbers"
+    it "checks for usual numbers" do
+      fizzbuzz.classify(17).should eq_to 17
+    end
+  end
+
 end
